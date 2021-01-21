be7a97209533ca71ad798b7d68d292cb83bed4b1
choidoorim <63203480+choidoorim@users.noreply.github.com>
2021-01-21 22:59:53 +0900

    dependency EX

--- src/main/java/hello/hellospring/sevice/MemberService.java
+++ src/main/java/hello/hellospring/sevice/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.sevice;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 //Ctrl + Shift + T 커맨드로 Test를 만들수 있다.
+//@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
     //회원가입
+    //@Autowired //Spring에 MemoryMemberRepoository를 넣어준다. helloController와 memberRepository 를 연결해주는 역할을 한다.
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
